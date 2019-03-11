2b595319e7f3da1c0f2d51f9564b34a030a96a93
Simon Vandeputte <simon.vandeputte@univ-lille.fr>
2019-03-11 17:48:01 +0100

    merde

---
+++ app/src/main/java/fil/ecostate/QuizzJour.java
@@ -0,0 +1,13 @@
+package fil.ecostate;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class QuizzJour extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quizzjour);
+    }
+}
